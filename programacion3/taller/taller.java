3af6535e2f9dd7b2402e906ba153ccbe1d03c2aa
alexissegura11 <jalexissegura@estudiante.uniajc.edu.co>
2026-09-14 21:27:05 +0000

    cambio1

---
+++ programacion3/taller/taller.java
@@ -0,0 +1,32 @@
+
+import java.util.random.*;
+
+
+//arreglos
+
+public class taller {
+    public static void main(String[] args) {
+        
+        
+        //declaracoin y creacion
+        int[] numeros = new int[7]; 
+        numeros[0] = 7; 
+        numeros[1] = 8;
+        numeros[2] = 9;
+        numeros[3] = 10;
+        numeros[4] = 11;
+        numeros[5] = 12;
+        numeros[6] = 13;
+    
+
+        System.out.println(numeros[0]);
+        System.out.println(numeros[1]);
+        System.out.println(numeros[2]);
+        System.out.println(numeros[3]);
+        System.out.println(numeros[4]);
+        System.out.println(numeros[5]);
+        System.out.println(numeros[6]);
+       
+
+    }
+}
